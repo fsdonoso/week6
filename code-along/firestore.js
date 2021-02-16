a92a7fd0375a5e22f708346fa2da66ca60c24400
Francisco Donoso <francisco.donoso@kellogg.northwestern.edu>
2021-02-15 22:45:49 -0600

    Last Version

--- code-along/firestore.js
+++ code-along/firestore.js
@@ -1,6 +1,39 @@
+
 document.addEventListener('DOMContentLoaded', async function(event) {
   // Step 1: Make the world's tiniest to-do app
+
+document.querySelector('form').addEventListener('submit', function(event) {
+  event.preventDefault()
+  
+  let todoText = document.querySelector('#todo').value
+
+  if (todoText.length >0) {
+
+  document.querySelector('.todos').insertAdjacentHTML('beforeend', `
+    <div class="py-4 text-x1 border-b-2 border-purple-500 w-full">
+      ${todoText}
+      </div>
+  `)
+  document.querySelector('#todo').value = ''
+}
+  
+})
+
   // Step 2: Read existing to-dos from Firestore
+
+  let db= firebase.firestore()
+  let querySnapshot = await db.collection('todos').get()
+  let todos = querySnapshot.docs
+  for (let i=0; i<todos.lenght; i++) {
+    let todoId = todos[i].id
+    let todoData = todos[i].data()
+    let todoText = todoData.text
+    console.log(todoText)
+  }
+
+
+  
+
   // Step 3: Add code to Step 1 to add todo to Firestore
   // Step 4: Add code to allow completing todos
 })
